84f34ba2ebb0d1367b50622fe7538409da4481b6
fred_2420@yahoo.co.uk <fred_2420@yahoomail.co.uk>
2019-06-21 19:54:55 +0200

    s

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var i = 0;
+var txt = '> Mesh Networking  \r\n \n > RTOS';
+var speed = 50;
+
+typeWriter();
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("Console").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+
 $.scrollify({
     section : "section",
     sectionName : "section-name",
@@ -16,3 +32,5 @@ $.scrollify({
     afterResize:function() {},
     afterRender:function() {}
   });
+
+
